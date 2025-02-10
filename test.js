f5ee58bc07bf2aa35d4dd2ffc603f17ee50dd5a5
Denys Vysockij <131520948+denyxqq@users.noreply.github.com>
2025-02-10 19:01:18 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,37 @@
+// #1
+// #2
+const ingredients = [
+  'Картопля',
+  'Гриби',
+  'Часник',
+  'Помідори',
+  'Зелень',
+  'Приправи',
+ ];
+ const ulElement =  document.querySelector("ul#ingredients");
+let liElement = [];
+for(let i = 0; i < ingredients.length; i++){
+liElement.push(`<li>${ingredients[i]}</li>`);;
+}
+const text = liElement.join(" ");
+
+ulElement.innerHTML = text;
+// #3
+const images = [
+  {
+   url:
+    'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+   alt: 'White and Black Long Fur Cat',
+  },
+  {
+   url:
+    'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+   alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+   url:
+    'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+   alt: 'Group of Horses Running',
+  },
+ ];
+//    #4
